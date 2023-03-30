9e1d9d33a06a43e70f6c22447085b0e18ea72962
luanpatrick10 <86279613+luanpatrick10@users.noreply.github.com>
2023-03-30 00:02:57 -0300

    Add project files.

---
+++ src/LP10.Doadores/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LP10.Doadores.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
